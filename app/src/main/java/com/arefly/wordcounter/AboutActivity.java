6bbaa272ceee51265c2586b94402564c6b15e753
Jason Ho <eflyjason@gmail.com>
2016-07-17 09:58:07 +0800

    add action bar to "About" PreferenceActivity add back button to "About" PreferenceActivity add "AppCompatPreferenceActivity"

--- app/src/main/java/com/arefly/wordcounter/AboutActivity.java
+++ app/src/main/java/com/arefly/wordcounter/AboutActivity.java
@@ -1,22 +1,50 @@
 package com.arefly.wordcounter;
 
 import android.os.Bundle;
-import android.preference.PreferenceActivity;
-import android.preference.PreferenceFragment;
+import android.support.v7.app.ActionBar;
+import android.view.MenuItem;
 
 import java.util.List;
 
-public class AboutActivity extends PreferenceActivity
+public class AboutActivity extends AppCompatPreferenceActivity
 {
     @Override
-    public void onBuildHeaders(List<Header> target)
-    {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setupActionBar();
+    }
+
+
+    @Override
+    public void onBuildHeaders(List<Header> target) {
         loadHeadersFromResource(R.xml.pref_headers, target);
     }
 
     @Override
-    protected boolean isValidFragment(String fragmentName)
-    {
+    protected boolean isValidFragment(String fragmentName) {
         return AboutFragment.class.getName().equals(fragmentName);
+    }
+
+    /**
+     * Set up the {@link android.app.ActionBar}, if the API is available.
+     */
+    private void setupActionBar() {
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            // Show the Up button in the action bar.
+            actionBar.setHomeButtonEnabled(true);
+        }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                // app icon in action bar clicked; goto parent activity.
+                this.finish();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
